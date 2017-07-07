26d42583910fd896487c9f8b83ad3d79764ac438
Samir Yuja <samiryuja@hotmail.com>
2017-07-07 12:36:46 -0400

    gtracks update

--- gtracks.py
+++ gtracks.py
@@ -6,11 +6,10 @@
     Script then Checks Arguments and Runs
 """
 
-#TODO: add subparsers!!
+#TODO: add commands and subparsers!!
 #TODO: how will user obtain application credentials?
 #BUG: -c must be provided at the very beginning for main parser to see it
 #BUG: else will have to add it do each subparser?
-#TODO: add more scripts
 
 import os
 import sys
@@ -41,7 +40,7 @@ def main():
 
 #parsing each possible subcommand using subparsers
 def argParseMain():
-    global AVAILABLE_SCRIPTS, ARGS
+    global ARGS
 
     #gtracks parser
     parser = argparse.ArgumentParser(prog="gtracks")
@@ -54,13 +53,23 @@ def argParseMain():
                 dest ="command")
 
     #parsers for each script
-    #makeHub
+    ##makeHub
     makeHub_parser = subparsers.add_parser("makeHubDb")
+    #spreadsheet name
     makeHub_parser.add_argument("hubDbname",help="Enter hubDb Spreadsheet Name")
 
     #addHub
     addHub_parser = subparsers.add_parser("addHub")
+    #spreadsheet name
     addHub_parser.add_argument("hubName", help="Enter hubName")
+    addHub_parser.add_argument("-s", "-shortLabel", help="Enter hubName")
+    addHub_parser.add_argument("-l", "-longLabel", help="Enter longlabel")
+    addHub_parser.add_argument("-g", "-genomesFile", default="genomes.txt")
+    addHub_parser.add_argument("-e", "-email", default="ex@example.com")
+    #TODO: remove default?
+    addHub_parser.add_argument("-u", "-url", default="",
+            help="Location of Hub on File Server")
+    #shortLabel, longLabel, genomesFile, email, url
 
     ARGS = parser.parse_args()
     #print "ARGS.command = %s" % ARGS.command
@@ -78,13 +87,13 @@ def getCredentialsArg():
 
 #call the appropriate script
 def selectScript(AUTH_GSPREAD_OBJ):
-    global AVAILABLE_SCRIPTS, ARGS
+    global ARGS
 
     #build function name
     function_str = ARGS.command + "." + ARGS.command + "Main" +\
         "(AUTH_GSPREAD_OBJ, ARGS)"
 
-    print "function_str = %s" % function_str
+    #BUG: print "function_str = %s" % function_str
 
     #call the function
     eval(function_str)
